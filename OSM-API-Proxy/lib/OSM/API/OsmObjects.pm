d11bee542efac47d118750d97e67123e291e5def
James Michael DuPont <JamesMikeDuPont@googlemail.com>
2011-07-29 21:27:55 +0200

    now without moose, even faster

--- OSM-API-Proxy/lib/OSM/API/OsmObjects.pm
+++ OSM-API-Proxy/lib/OSM/API/OsmObjects.pm
@@ -1,66 +1,104 @@
 package OSM::API::OsmObjects::Node;
-use Moose;
-use MooseX::Types::DateTimeX qw( DateTime );
+#use Moose;
+#use MooseX::Types::DateTimeX qw( DateTime );
 use Geo::Hash;
 
 
 my $gh = Geo::Hash->new;
 
-has 'id' =>
-    is => "rw",
-    isa => "Num",
-    ;
+# has 'id' =>
+#     is => "rw",
+#     isa => "Num",
+#     ;
 
-has 'timestamp' =>
-    is => "rw",
-#    isa => "DateTime",
-    isa => "Str",
-    ;
+# has 'timestamp' =>
+#     is => "rw",
+# #    isa => "DateTime",
+#     isa => "Str",
+#     ;
 
-# what part of the osm planet is this
-has 'partno' =>
-    is => "rw",
-    isa => "Num",
-    ;
+# # what part of the osm planet is this
+# has 'partno' =>
+#     is => "rw",
+#     isa => "Num",
+#     ;
 
-has 'user' =>
-    is => "rw",
-    isa => "Str",
-    ;
+# has 'user' =>
+#     is => "rw",
+#     isa => "Str",
+#     ;
 
-has 'visible' =>
-    is => "rw",
-    isa => "Str", #true
-    ;
+# has 'visible' =>
+#     is => "rw",
+#     isa => "Str", #true
+#     ;
 
-has 'version' =>
-    is => "rw",
-    isa => "Num", #true
-    ;
+# has 'version' =>
+#     is => "rw",
+#     isa => "Num", #true
+#     ;
 
-has 'changeset' =>
-    is => "rw",
-    isa => "Num", #true
-    ;
+# has 'changeset' =>
+#     is => "rw",
+#     isa => "Num", #true
+#     ;
 
-has 'lat' =>
-    is => "rw",
-    isa => "Num", #true
-    ;
+# has 'lat' =>
+#     is => "rw",
+#     isa => "Num", #true
+#     ;
 
-has 'lon' =>
-    is => "rw",
-    isa => "Num", #true
-    ;
+# has 'lon' =>
+#     is => "rw",
+#     isa => "Num", #true
+#     ;
 
-has 'hash' =>
-     is => "rw",
-     isa => "Str", #true
-    ;
+# has 'hash' =>
+#      is => "rw",
+#      isa => "Str", #true
+#     ;
 
-#has_element 'tags' =>
-#    is => "ro",
+# #has_element 'tags' =>
+# #    is => "ro",
 
+sub lat
+{
+    my $self=shift;
+    return $self->{lat};
+}
+
+sub hash
+{
+    my $self=shift;
+    my $val =shift;
+    if ($val)
+    {
+	$self->{hash}=$val;
+    }
+    else
+    {
+	return $self->{hash};
+    }
+}
+
+sub lon
+{
+    my $self=shift;
+    return $self->{lon};
+}
+
+sub partno
+{
+    my $self=shift;
+    return $self->{partno};
+}
+
+sub new 
+{
+    my $class=shift;
+    my $self =shift;
+    return bless $self,$class;
+}
 
 sub Hash
 {
@@ -76,7 +114,7 @@ sub ProcessHistory
     $self->Hash();
 }
 
-use YAML;
+#use YAML;
 use File::Path qw(make_path remove_tree);
 sub Split
 {
